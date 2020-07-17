4ba66e37ea60ab5137c813b8fa413f3a1ef6d271
Matt <mattiassalicru@gmail.com>
2020-07-17 12:07:45 -0300

    totally redesign

--- main.js
+++ main.js
@@ -1,68 +1,36 @@
+const addTask = document.querySelector('#submit-task'); //submit button
+const inputValue = document.querySelector('#input-form'); //input value
+const errorMsg = document.querySelector('#msg'); //Error Message
 
-const newTaskButton = document.getElementById('new-task');
-newTaskButton.innerHTML = 'New task!';
+addTask.addEventListener("click", onSubmit);
 
-const addTask = document.getElementById('add-task');
-addTask.innerHTML = 'Add Task';
-
-const form = document.getElementById('form');
-
-let taskList = [];
-
-newTaskButton.addEventListener('click', openTasksForm);
-
-addTask.addEventListener('click', sendTask)
-
-function openTasksForm(){
-    
-    if(form.style.display === 'none'){
-        form.style.display = 'flex';
-        newTaskButton.innerHTML = 'Cancel new task';
-    } else if(form.style.display != 'none'){
-        form.style.display = 'none';
-        newTaskButton.innerHTML = 'New task!';
-    }
+function error () {
+    errorMsg.innerHTML = '<h6 class="error"> Please complete all fields </h6>';
 }
 
-function sendTask(e) {
+function onSubmit(e) {
     e.preventDefault();
 
-    document.getElementById('tasks').innerHTML = ''; // Limpia el array antes de imprimirlo todo de vuelta.
+    if(inputValue.value === ''){ 
+    
+    setTimeout(error(), 3000);
+    
+    } else {
 
-    let inputTask = document.getElementById('input-form').value; //Guarda el input del usuario.
+        const taskList = document.querySelector('#task-list');
+        const li = document.createElement('li');
+        li.className = 'item';
 
-    if (inputTask != '') {
-        
-        taskList.push(inputTask);
-        
-        taskList.forEach((item, index) => {
-            document.getElementById('tasks').innerHTML += `
-        <p id='to-do' class='task-list'> <input id='check' type='checkbox'>  ${taskList[index]} </input> </p>
-        `; 
+        li.addEventListener('click', () => {
+            li.classList.toggle('item-checked');
         });
 
-        document.querySelectorAll('#to-do').forEach((item) => {
-            item.addEventListener('change', checked)    
-        })
-
-    } else {
-        alert('First you must write a task to do!')
+        li.appendChild(document.createTextNode(`${inputValue.value}`));
+        taskList.appendChild(li);
+        inputValue.value = '';
     }
 }
 
 
-function checked() {
 
-    document.querySelectorAll('#check').forEach(() => {
-        
-            this.style.textDecoration = 'line-through';
-            this.style.fontStyle = 'italic';
-            this.style.color = '#45454590';
-        
-            // this.style.textDecoration = 'none';
-            // this.style.fontStyle = 'normal';
-            // this.style.color = '#000';
-        }
-        
-    )    
-}
+
