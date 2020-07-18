841513b47b559df9de856bbed31ee6f5fb2e47ac
Matt <mattiassalicru@gmail.com>
2020-07-18 13:49:07 -0300

    local Storage for the tasks in the list

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 const addTask = document.querySelector('#submit-task'); //submit button
 const inputValue = document.querySelector('#input-form'); //input value
 const errorMsg = document.querySelector('#msg'); //Error Message
+const taskList = document.querySelector('#task-list'); //El formulario
+let itemsArray = localStorage.getItem('items')
+    ? JSON.parse(localStorage.getItem('items'))
+    : [];
 
 addTask.addEventListener("click", onSubmit);
 
 function error() {
     errorMsg.innerHTML = '<h6 class="error"> Please write a task </h6>';
 }
+
+const liMaker = (text) => {
+
+    const li = document.createElement('li');
+    li.className = 'item';
+
+    const trash = document.createElement('div');
+
+    const listIcon = document.createElement('div');
+    listIcon.className = 'listIcon';
+
+    li.addEventListener('click', () => {
+        li.classList.toggle('item-checked');
+        trash.classList.toggle('trash');
+    });
+
+    trash.addEventListener('click', () => {
+        li.remove();
+        itemsArray.splice(itemsArray.indexOf(text), 1);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+    });
+
+    li.appendChild(listIcon);
+    li.appendChild(document.createTextNode(`${text}`));
+    li.appendChild(trash);
+    taskList.appendChild(li);
+
+}
+
 
 function onSubmit(e) {
     e.preventDefault();
@@ -18,30 +51,23 @@ function onSubmit(e) {
         }, 2000);
     } else {
 
-        const taskList = document.querySelector('#task-list');
+        itemsArray.push(inputValue.value);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(inputValue.value);        
 
-        const li = document.createElement('li');
-        li.className = 'item';
-
-        const trash = document.createElement('div');
-        // trash.className = 'trash';
-
-        const listIcon = document.createElement('div');
-        listIcon.className = 'listIcon';
-
-        li.addEventListener('click', () => {
-            li.classList.toggle('item-checked');
-            trash.classList.toggle('trash');
-        });
-
-        trash.addEventListener('click', () => {
-            li.remove();
-        });
-
-        li.appendChild(listIcon);
-        li.appendChild(document.createTextNode(`${inputValue.value}`));
-        li.appendChild(trash);
-        taskList.appendChild(li);
         inputValue.value = '';
+
+        console.log(itemsArray)
     }
 }
+
+
+//LOCAL STORAGE
+
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+data.forEach((item) => {
+    liMaker(item)
+})
